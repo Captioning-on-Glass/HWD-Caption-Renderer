e319e9a55550557a7262dd2f4f2a62cb187cf2db
Gabriel Britain <quontas@gmail.com>
2021-11-08 14:17:59 -0500

    Cleaned up imports

--- app/src/main/java/edu/gatech/cog/ipglasses/renderingmethods/FocusedSpeakerAndGlobalRenderer.kt
+++ app/src/main/java/edu/gatech/cog/ipglasses/renderingmethods/FocusedSpeakerAndGlobalRenderer.kt
@@ -1,9 +1,9 @@
 package edu.gatech.cog.ipglasses.renderingmethods
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
