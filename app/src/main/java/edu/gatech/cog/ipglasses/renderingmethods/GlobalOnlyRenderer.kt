402bd7cf72dcc27191472ec763d7631c7308e672
Atush <atush.d@gmail..com>
2021-11-08 22:50:53 -0500

    Presentation Method #2

---
+++ app/src/main/java/edu/gatech/cog/ipglasses/renderingmethods/GlobalOnlyRenderer.kt
@@ -0,0 +1,84 @@
+package edu.gatech.cog.ipglasses.renderingmethods
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.gatech.cog.ipglasses.CaptionMessage
+import edu.gatech.cog.ipglasses.CaptioningViewModel
+import edu.gatech.cog.ipglasses.Renderers
+import edu.gatech.cog.ipglasses.Speakers
+import edu.gatech.cog.ipglasses.ui.theme.IPGlassesTheme
+
+
+private const val TAG = "GlobalOnlyRenderer"
+
+@Preview(showBackground = false, widthDp = 480, heightDp = 480)
+@Composable
+fun GlobalOnlyRenderer() {
+    val viewModel = CaptioningViewModel()
+    viewModel.renderingMethodToUse = Renderers.GLOBAL_ONLY
+    val lipsum = LoremIpsum(1)
+    for ((i, chunk) in lipsum.values.take(4).iterator().withIndex()) {
+        viewModel.addMessage(
+            CaptionMessage(
+                messageId = i,
+                chunkId = 0,
+                text = chunk,
+                speakerId = "",
+                focusedId = ""
+            )
+        )
+    }
+
+    IPGlassesTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            GlobalOnlyRenderer(viewModel)
+        }
+    }
+}
+
+/**
+ * Renders all spoken text, labeling each chunk with who said what.
+ * @param viewModel: The [CaptioningViewModel] to use as a single source of truth for captions.
+ */
+@Composable
+fun GlobalOnlyRenderer(viewModel: CaptioningViewModel) {
+    val globalCaptionMessages = viewModel.globalCaptionMessages.value
+    val textToDisplay = if (globalCaptionMessages.isEmpty()) {
+        ""
+    } else {
+        val sortedMessagesMap = globalCaptionMessages.groupBy { it.messageId }
+            .toSortedMap() // Group all the captions we have so far by messageId
+        val latestMessage: List<CaptionMessage> =
+            sortedMessagesMap[sortedMessagesMap.lastKey()]!!
+        val messageText =
+            latestMessage.sortedBy { captionMessage -> captionMessage.chunkId }
+                .joinToString(" ") { message -> message.text }
+        "$messageText"
+    }
+    Box(
+        modifier = Modifier
+            .padding(30.dp)
+            .fillMaxSize()
+    ) {
+        LimitedText(
+            modifier = Modifier.align(Alignment.BottomStart),
+            maxBottomLines = 100,
+            fontSize = 28.sp,
+            text = textToDisplay,
+            color = Color.White,
+        )
+    }
+}
